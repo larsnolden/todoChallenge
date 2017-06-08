680a0365be75ecf6a2064c66bbdbff803dfb6172
Lars Nolden <larsnolden@web.de>
2017-06-08 17:12:03 -0400

    v1 of localStorage

--- client.js
+++ client.js
@@ -7,6 +7,23 @@ input.addEventListener('keypress', (event) => {
     if (event.keyCode === 13) add()
 })
 
+if (localStorage.todos) renderAll(fromLocal(localStorage.todos));
+
+// NOTE: These are listeners for events from the server
+// This event is for (re)loading the entire list of todos from the server
+server.on('load', (todos) => {
+    clean()
+    console.log(todos)
+    console.log(toLocal(todos))
+    localStorage.todos = toLocal(todos)
+    renderAll(todos)
+});
+
+server.on('newTodo', (todo) => {
+    localStorage.todos.push(todo);
+    render(todo)
+});
+
 //emit new todo to server
 function add() {
     console.warn(event);
@@ -33,18 +50,18 @@ function render(todo) {
     list.append(listItem);
 }
 
+function renderAll(todos) {
+    todos.forEach((todo) => render(todo));
+}
+
+function toLocal(value) {
+    return JSON.stringify(value)
+}
+
+function fromLocal(string) {
+    return JSON.parse(string)
+}
+
 function clean() {
     list.innerHTML = '';
 }
-
-// NOTE: These are listeners for events from the server
-// This event is for (re)loading the entire list of todos from the server
-server.on('load', (todos) => {
-    clean()
-    console.log(`received new set of todos: ${JSON.stringify(todos)}`)
-    todos.forEach((todo) => render(todo));
-});
-
-server.on('newTodo', (todo) => {
-    render(todo)
-});
