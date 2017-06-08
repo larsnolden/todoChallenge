d9b71e5badd4f20c635eafa9ff38a0fd0784690e
Lars Nolden <larsnolden@web.de>
2017-06-08 17:36:20 -0400

    localStorage fully working

--- client.js
+++ client.js
@@ -12,16 +12,13 @@ if (localStorage.todos) renderAll(fromLocal(localStorage.todos));
 // NOTE: These are listeners for events from the server
 // This event is for (re)loading the entire list of todos from the server
 server.on('load', (todos) => {
-    clean()
-    console.log(todos)
-    console.log(toLocal(todos))
-    localStorage.todos = toLocal(todos)
     renderAll(todos)
+    localStorage.todos = toLocal(todos)
 });
 
 server.on('newTodo', (todo) => {
-    localStorage.todos.push(todo);
     render(todo)
+    localStorage.todos = toLocal(fromLocal(localStorage).push(todo));
 });
 
 //emit new todo to server
@@ -43,25 +40,27 @@ function add() {
 
 //render single Todo
 function render(todo) {
-    console.log(todo);
     const listItem = document.createElement('li');
     const listItemText = document.createTextNode(todo.title);
     listItem.appendChild(listItemText);
     list.append(listItem);
 }
 
+//render Todos
 function renderAll(todos) {
+    cleanTodos()
     todos.forEach((todo) => render(todo));
 }
 
+function cleanTodos() {
+    list.innerHTML = '';
+}
+
+//helper to store objects in localStorage
 function toLocal(value) {
     return JSON.stringify(value)
 }
 
 function fromLocal(string) {
     return JSON.parse(string)
-}
-
-function clean() {
-    list.innerHTML = '';
 }
